4f404e20b35e5adb67b0db14e4373d4e2afe0bf3
Rémy Pottiez <rpottiez.pro@gmail.com>
2021-04-25 19:34:40 +0200

    Reboot

---
+++ projetEsport/projetEsport/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using projetEsport.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projetEsport.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Licencie> Licencie { get; set; }
+        public DbSet<Equipe> Equipe { get; set; }
+        public DbSet<Jeu> Jeu { get; set; }
+        public DbSet<TypeCompetition> TypeCompetition { get; set; }
+        public DbSet<Competition> Competition { get; set; }
+        public DbSet<ClassementCompetition> ClassementCompetition { get; set; }
+    }
+}
---
+++ projetEsport/projetEsport/Models/Licencie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetEsport.Models
+{
+    public class Licencie
+    {
+        #region Propriétés
+        public int ID { get; set; }
+        [Display(Name = "Pseudo")]
+        public String Pseudo { get; set; }
+        [Display(Name = "Prénom")]
+        public String Penom { get; set; }
+        [Display(Name = "Nom")]
+        public String Nom { get; set; }
+        [Display(Name = "Identifiant utilisateur")]
+        public String IdUtilisateur { get; set; }
+        [Display(Name = "Créé le")]
+        public DateTime CreeLe { get; set; }
+        [Display(Name = "Modifié le")]
+        public DateTime ModifieeLe { get; set; }
+        public bool PremierConnexion { get; set; }
+        #endregion
+
+        #region Clées
+        [Display(Name = "Numéro de l'équipe")]
+        public int? EquipeID { get; set; }
+        [Display(Name = "Equipe")]
+        public Equipe? Equipe { get; set; }
+        #endregion
+    }
+}

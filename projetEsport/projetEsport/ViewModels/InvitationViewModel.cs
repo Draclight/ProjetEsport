3556b56de326ea2430c1c2d5ee178cd1b97a4f89
Rémy Pottiez <rpottiez.pro@gmail.com>
2021-05-13 20:04:39 +0200

    Invitation dans une équipe

---
+++ projetEsport/projetEsport/ViewModels/InvitationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projetEsport.ViewModels
+{
+    public class InvitationViewModel
+    {
+        public int ID { get; set; }
+        public int LicencieID { get; set; }
+        [Display(Name = "Equipe")]
+        public string NomEquipe { get; set; }
+        public IList<string> Membres { get; set; }
+        [Display(Name = "Reçu")]
+        public DateTime Quand { get; set; }
+        public bool Accepter { get; set; }
+    }
+}
